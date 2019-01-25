3d5f0d2b71be775ea961597bdd852e384d172db9
Andrey Bannikov <andrey27x777@gmail.com>
2019-01-25 05:33:15 +0200

    Added FeedbackForm component in Works page

---
+++ src/frontend/components/Forms/FeedbackForm.vue
@@ -0,0 +1,92 @@
+<template lang="pug">
+  section#feedback.section.container.container-full_width
+
+    FormWrapper(
+      :addClasses="containerClasses"
+      @submit.prevent.native="handleSubmit")
+
+      h2.section__title.section__title-uppercase.section__title-underlined.feedback_form__title Связаться со мной
+
+      //- .form__row.form__row-text_wrap.feedback_form__row
+      //-   input.form__input.feedback_form__text_input(type="text" name="name")
+
+      InputEventElem(
+        v-model="name"
+        :val="$v.name"
+        placeholder="Пользователь")
+
+      //- .form__row.form__row-text_wrap.feedback_form__row
+      //-   input.form__input.feedback_form__text_input(type="email" name="email")
+
+      InputEventElem(
+        type="email"
+        v-model="email"
+        :val="$v.email"
+        placeholder="Почта")
+
+      //- .form__row.form__row-textarea_wrap
+      //-   textarea.form__input.feedback_form__text_input(name="message")
+
+      InputEventElem(
+        type="textarea"
+        v-model="message"
+        :val="$v.message"
+        placeholder="Сообщение")
+
+      .menu.form__menu.feedback_form__menu
+        input.menu__link.btn(type="submit", value="Отправить")
+        input.menu__link.btn.btn-full_opacity(type="reset" value="Очистить")
+        //- li.menu__item
+        //-   input.menu__link.btn(type="submit", value="Отправить")
+        //- li.menu__item
+        //-   input.menu__link.btn.btn-full_opacity(type="reset" value="Очистить")
+</template>
+
+<script>
+import {
+  required,
+  alphaNum,
+  email,
+  minLength,
+  maxLength
+} from "vuelidate/lib/validators";
+import FormWrapper from "@components/FormElems/FormWrapper";
+import InputEventElem from "@components/FormElems/InputEventElem";
+
+export default {
+  name: "FeedbackForm",
+  components: {
+    FormWrapper,
+    InputEventElem
+  },
+  data() {
+    return {
+      name: "",
+      email: "",
+      message: ""
+    };
+  },
+  computed: {
+    containerClasses() {
+      return {
+        feedback_form: true
+      };
+    }
+  },
+  validations: {
+    name: {
+      required,
+      alphaNum
+    },
+    email: {
+      required,
+      email
+    },
+    message: {
+      required,
+      minLength: minLength(27),
+      maxLength: maxLength(527)
+    }
+  }
+};
+</script>
--- src/frontend/views/Works.vue
+++ src/frontend/views/Works.vue
@@ -1,18 +1,46 @@
 <template lang="pug">
   FullPageWrapper
     TopWrapper
+      SectionWrapper(name="slider" :isFullWidth="true" :isOwnContainerClass="true")
+        ul.slider__list
+          li.slider__item.slider__item-active
+          li.slider__item
+          li.slider__item
+          li.slider__item
+        
+        article.slider__text_wrap
+          h2.section__title.section__title-uppercase.section__title-underlined.slider__title Сайт школы онлайн Образования
+          p.section__desc.slider__desc HTML , CSS, JAVASCRIPT
+          a.clearfix.btn.slider__btn(href="")
+            span.icon.icon-link.slider__btn_icon
+            span.slider__btn_text Посмотреть сайт
+
+        .clearfix.slider__nav
+          a.icon.icon-chevron_down.slider__arrow.slider__arrow-prev(href="")
+          a.icon.icon-chevron_up.slider__arrow.slider__arrow-next(href="")
+
+    BottomWrapper(:addClasses="{'bottom_wrap-bg': true}")
+      FeedbackForm
 </template>
 
 <script>
 import pageConfig from "@frontend/mixins/pageConfig";
 import FullPageWrapper from "@frontCmp/FullPageWrapper";
 import TopWrapper from "@frontCmp/TopWrapper";
+import SectionWrapper from "@frontCmp/SectionWrapper";
+import TitleWrapper from "@frontCmp/TitleWrapper";
+import BottomWrapper from "@frontCmp/BottomWrapper";
+import FeedbackForm from "@frontCmp/Forms/FeedbackForm";
 
 export default {
   name: "WorksView",
   components: {
     FullPageWrapper,
-    TopWrapper
+    TopWrapper,
+    SectionWrapper,
+    TitleWrapper,
+    BottomWrapper,
+    FeedbackForm
   },
   mixins: [pageConfig],
   data() {

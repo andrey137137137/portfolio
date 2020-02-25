71f6a905ffdfa31d5c4ce62a4873749803cf9efd
andrey137137137 <andrey27x777@gmail.com>
2020-02-25 10:32:15 +0300

    Remoted package-lock.json

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -32,6 +32,7 @@ const User = mongoose.model("user");
 module.exports.isAuth = (req, res, next) => {
   console.log(req.session);
   console.log(req.body);
+
   if (req.session.user) {
     return next();
   }

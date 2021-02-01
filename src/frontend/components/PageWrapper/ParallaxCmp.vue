c71df941f22270260bdeb6fc6ab4e74d1b4af42d
Andrey Bannikov <andrey27x777@gmail.com>
2021-02-01 13:59:29 +0200

    Modified Parallax calculation

--- src/frontend/components/PageWrapper/ParallaxCmp.vue
+++ src/frontend/components/PageWrapper/ParallaxCmp.vue
@@ -1,13 +1,12 @@
 <template lang="pug">
-.parallax(:class='classes')
+.parallax(ref='lkfsdjdlfglk', :class='classes')
   ImageWrapper.parallax-layer(
-    v-for='(item, index) in layers',
+    v-for='number in count',
+    :key='number',
     ref='layers',
-    :key='index',
-    :data-depth='calcDivider(index)',
-    :path='getLayerPath(index)',
+    :path='getLayerPath(number)',
     :breakpoints='breakpoints',
-    :title='getTitle(index)',
+    :title='getTitle(number)',
     :isWrapperClass='false',
     :imgAddClasses='{ "parallax-img": true }',
     :isLazyLoading='false'
@@ -28,8 +27,8 @@ export default {
     return {
       centerX: 0,
       centerY: 0,
-      initialX: 0,
-      initialY: 0,
+      diffX: 0,
+      diffY: 0,
       scrollY: 0,
       divider: 0,
       positionX: 0,
@@ -38,19 +37,16 @@ export default {
       transformString: '',
       path: '/upload/parallax',
       ext: 'png',
-      layers: [100, 100, 90, 80, 70, 60, 15, 10],
       breakpoints: [
         { name: 'xlg.png', value: 2000 },
         { name: 'lg.png', value: 1200 },
         { name: 'md.png', value: 768 },
         { name: 'sm.png', value: 0 },
       ],
+      count: 3,
     };
   },
   computed: {
-    areSomeLayers() {
-      return this.$refs.layers[1];
-    },
     isScroll() {
       return this.$route.name != HOME.name;
     },
@@ -59,8 +55,11 @@ export default {
     },
   },
   methods: {
-    calcDivider(index) {
-      return (this.$refs.layers.length - index) * 10;
+    areSomeLayers() {
+      return this.$refs.layers[1];
+    },
+    calcDivider(index, mult, divider) {
+      return ((this.$refs.layers.length - index) * mult) / divider;
     },
     getLayerPath(index) {
       return this.path + '/layer_' + index;
@@ -69,34 +68,33 @@ export default {
       return 'Слой ' + index;
     },
     moveLayers(event) {
-      if (this.areSomeLayers) {
+      if (this.areSomeLayers()) {
         const $vm = this;
 
         if ($vm.isScroll) {
           $vm.scrollY = getScrollY();
+        } else {
+          $vm.diffX = $vm.centerX - event.pageX;
         }
 
-        $vm.initialX = $vm.isScroll ? $vm.centerX : $vm.centerX - event.pageX;
-        $vm.initialY = $vm.isScroll
-          ? $vm.centerY - $vm.scrollY
-          : $vm.centerY - event.pageY;
+        $vm.diffY = $vm.isScroll ? -$vm.scrollY : $vm.centerY - event.pageY;
 
-        $vm.$refs.layers.forEach($layerCmp => {
+        $vm.$refs.layers.forEach(($layerCmp, index) => {
           const $layer = $layerCmp.$el;
 
           if ($vm.isScroll) {
-            $vm.divider = $layer.dataset.depth / 90;
+            $vm.divider = $vm.calcDivider(index, 25, 100);
           } else {
-            $vm.divider = $layer.dataset.depth / 10000;
+            $vm.divider = $vm.calcDivider(index, 50, 10000);
           }
 
-          $vm.positionY = $vm.initialY * $vm.divider;
+          $vm.positionY = $vm.diffY * $vm.divider;
           $vm.bottomPosition = $vm.centerY * $vm.divider;
 
           if ($vm.isScroll) {
             $vm.transformString = `translateY(${$vm.positionY}px)`;
           } else {
-            $vm.positionX = $vm.initialX * $vm.divider;
+            $vm.positionX = $vm.diffX * $vm.divider;
             $vm.transformString = `translate(${$vm.positionX}px, ${$vm.positionY}px)`;
           }
 
@@ -109,23 +107,27 @@ export default {
   mounted() {
     const $vm = this;
 
-    $vm.centerX = window.innerWidth / 2;
-    $vm.centerY = window.innerHeight / 2;
+    $vm.$nextTick(() => {
+      $vm.centerX = window.innerWidth / 2;
+      $vm.centerY = window.innerHeight / 2;
 
-    if ($vm.areSomeLayers) {
-      // if ($vm.$parallaxContainer.classList.contains("parallax--scroll")) {
-      //   $vm.isScroll = true;
-      // }
+      for (; !$vm.$refs.layers; );
 
-      // $container = $vm.$parallaxContainer.firstElementChild
+      if ($vm.areSomeLayers) {
+        // if ($vm.$parallaxContainer.classList.contains("parallax--scroll")) {
+        //   $vm.isScroll = true;
+        // }
 
-      if ($vm.isScroll) {
-        window.addEventListener('scroll', $vm.moveLayers);
-        window.dispatchEvent(new Event('scroll'));
-      } else {
-        window.addEventListener('mousemove', $vm.moveLayers);
+        // $container = $vm.$parallaxContainer.firstElementChild
+
+        if ($vm.isScroll) {
+          window.addEventListener('scroll', $vm.moveLayers);
+          window.dispatchEvent(new Event('scroll'));
+        } else {
+          window.addEventListener('mousemove', $vm.moveLayers);
+        }
       }
-    }
+    });
   },
 };
 </script>

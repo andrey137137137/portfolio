617d2431335ebeff7951ed632cd21032ffde01e0
Bannikov Andrey <andrey27x777@gmail.com>
2021-11-12 04:12:04 +0200

    Refactoring preloader

--- src/frontend/components/PageWrapper/PreloaderCmp.vue
+++ src/frontend/components/PageWrapper/PreloaderCmp.vue
@@ -1,17 +1,18 @@
 <template lang="pug">
-div(ref='preloader', :class='rootClass')
+div(ref='preloader', :class='getRootClasses()')
   .preloader-container(:class='isChangedPage')
     svg.preloader-circles
       circle.preloader-center_circle(ref='circle')
       circle.preloader-satellite_circle(ref='satellite')
     .preloader-counter(ref='counter') {{ animatedNumber }}
+  //- div(ref='preloader', :class='rootClass')
 </template>
 
 <script>
 // import { gsap } from 'gsap';
 import { mapGetters } from 'vuex';
 import {
-  IMAGES_LOADING,
+  // IMAGES_LOADING,
   PRELOADER_CLASSES_REMOVING,
   PRELOADER_HIDDEN,
 } from '@common/constants/timeouts';
@@ -22,11 +23,12 @@ export default {
     return {
       rootClass: 'preloader',
       activeClass: 'preloader--active',
-      intervalID: 0,
-      imageCount: 1000,
+      // intervalID: 0,
+      imageCount: 100000000000,
       imageCounter: 0,
       prs: 0,
       tweenedNumber: 0,
+      isFirstLoading: true,
     };
   },
   computed: {
@@ -53,46 +55,51 @@ export default {
     },
   },
   methods: {
-    startLoading() {
-      const $vm = this;
-
-      $vm.$refs.preloader.classList.add(this.activeClass);
-
-      this.intervalID = setInterval(() => {
-        $vm.imagesLoading();
-      }, IMAGES_LOADING);
+    getRootClasses() {
+      return this.rootClass + ' ' + this.activeClass;
     },
+    // startLoading() {
+    //   const $vm = this;
+
+    //   $vm.$refs.preloader.classList.add($vm.activeClass);
+
+    //   $vm.intervalID = setInterval(() => {
+    //     $vm.imagesLoading();
+    //   }, IMAGES_LOADING);
+    // },
     reset() {
-      if (this.dbDataLoadedCount == 0) {
-        this.imageCount = 1000;
-        this.imageCounter = 0;
-        this.prs = 0;
-        this.tweenedNumber = 0;
-        this.$refs.preloader.style.display = 'block';
-        this.startLoading();
-      }
+      // if (this.dbDataLoadedCount == 0) {
+      this.imageCount = 100000000000;
+      this.imageCounter = 0;
+      this.prs = 0;
+      this.tweenedNumber = 0;
+      this.$refs.preloader.style.display = 'block';
+      // this.startLoading();
+      // }
+      this.$refs.preloader.classList.add(this.activeClass);
+      this.imagesLoading();
     },
     imagesLoading() {
       if (this.dbDataLoadingCount == this.dbDataLoadedCount) {
-        clearInterval(this.intervalID);
+        // clearInterval(this.intervalID);
         const $vm = this;
 
-        // $vm.$nextTick(() => {
-        const $images = document.querySelectorAll(
-          'source:not(.lazy), img:not(.lazy)',
-        );
-        $vm.imageCount = $images.length;
-        console.log('Reset');
+        $vm.$nextTick(() => {
+          const $images = document.querySelectorAll(
+            'source:not(.lazy), img:not(.lazy)',
+          );
+          $vm.imageCount = $images.length;
+          console.log('imagesLoading');
 
-        for (let i = 0; i < $vm.imageCount; i++) {
-          console.log(i);
-          console.log($vm.imageCount);
-          const $imageClone = new Image();
-          $imageClone.onload = $vm.imageLoaded;
-          $imageClone.onerror = $vm.imageLoaded;
-          $imageClone.src = $images[i].src;
-        }
-        // });
+          for (let i = 0; i < $vm.imageCount; i++) {
+            console.log(i);
+            console.log($vm.imageCount);
+            const $imageClone = new Image();
+            $imageClone.onload = $vm.imageLoaded;
+            $imageClone.onerror = $vm.imageLoaded;
+            $imageClone.src = $images[i].src;
+          }
+        });
       }
     },
     imageLoaded() {
@@ -122,17 +129,30 @@ export default {
       // });
       this.tweenedNumber = newValue;
     },
+    dbDataLoadingCount() {
+      this.imagesLoading();
+    },
+    dbDataLoadedCount(newValue) {
+      if (!newValue && !this.isFirstLoading) {
+        this.reset();
+      } else {
+        if (this.isFirstLoading) {
+          this.isFirstLoading = false;
+        }
+        this.imagesLoading();
+      }
+    },
   },
-  mounted() {
-    console.log(this.$options.name + ' mounted');
-    this.startLoading();
-  },
-  beforeUpdate() {
-    if (!this.isActive) {
-      console.log(this.$options.name + ' beforeUpdate');
-      this.reset();
-    }
-  },
+  // mounted() {
+  //   console.log(this.$options.name + ' mounted');
+  //   this.startLoading();
+  // },
+  // beforeUpdate() {
+  //   if (!this.isActive) {
+  //     console.log(this.$options.name + ' beforeUpdate');
+  //     this.reset();
+  //   }
+  // },
 };
 </script>
 

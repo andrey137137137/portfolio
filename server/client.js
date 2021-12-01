195d71331e053fb4fc3e28084b5036f5806ef214
Bannikov Andrey <andrey27x777@gmail.com>
2021-12-02 00:45:17 +0200

    Modified mail template

--- server/client.js
+++ server/client.js
@@ -1,6 +1,6 @@
 module.exports = app => {
   const { IS_DEV } = require('@apiHelpers');
-  const PUBLIC_PATTERN = '/*';
+  const ALL_PATTERN = '/*';
 
   if (!IS_DEV) {
     const path = require('path');
@@ -8,17 +8,17 @@ module.exports = app => {
 
     app.use(require('express').static(BUILT_CLIENT));
 
-    app.get('/admin' + PUBLIC_PATTERN, (req, res) => {
+    app.get('/admin' + ALL_PATTERN, (req, res) => {
       res.sendFile(path.resolve(BUILT_CLIENT, 'admin.html'));
     });
-    app.get(PUBLIC_PATTERN, (req, res) => {
+    app.get(ALL_PATTERN, (req, res) => {
       res.sendFile(path.resolve(BUILT_CLIENT, 'index.html'));
     });
   } else {
     // catch 404 and forward to error handler
     const { NOT_FOUND } = require('@httpSt');
 
-    app.get(PUBLIC_PATTERN, (req, res) => {
+    app.get(ALL_PATTERN, (req, res) => {
       res.status(NOT_FOUND).json({ message: 'Not Found', error: NOT_FOUND });
     });
   }
--- server/routes/comment/mailTemplate.js
+++ server/routes/comment/mailTemplate.js
@@ -3,18 +3,12 @@ import App from './letterTemplate.vue';
 
 // экспортируем функцию фабрику для создания экземпляров
 // нового приложения
-function createApp(context) {
-  const app = new Vue({
+export default context => {
+  return new Vue({
     data: {
       item: context.item,
     },
     // корневой экземпляр просто рендерит компонент App
     render: h => h(App),
   });
-  return { app };
-}
-
-export default context => {
-  const { app } = createApp(context);
-  return app;
 };
